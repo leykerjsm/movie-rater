9357d0d42e8cfa28124de79653019e98ebe5f8bf
Leyker <leykerjsm@gmail.com>
2025-05-06 20:33:05 -0300

    Prepare for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      protocolImports: true,
-    }),
-  ],
+  plugins: [react()],
   define: {
     'process.env': {}
   },
   build: {
-    rollupOptions: {
-      plugins: [
-        nodePolyfills()
-      ]
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
+    outDir: 'dist',
+    emptyOutDir: true,
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-    },
+  server: {
+    port: 3000,
   },
+  preview: {
+    port: 3000,
+  }
 })
