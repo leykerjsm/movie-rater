45d435d02632849d5295cc05a6905df624aae4ac
Leyker <leykerjsm@gmail.com>
2025-05-06 20:23:43 -0300

    Update build configuration with Node.js polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true,
+    }),
+  ],
+  define: {
+    'process.env': {}
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodePolyfills()
+      ]
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
 })
